12b42d10c01ad3f71cda55bb3b817d5e824320f4
alainjurado91 <e.alain.jurado@gmail.com>
2024-01-22 22:46:43 -0600

    primer commit del ejercicio en clase

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native'
 
-export default function App() {
-  return (
+const App = () => {
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hola Mundo!</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
+export default App 
+
+const styles = StyleSheet.create ({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    flex: 1
   },
-});
+  text: {
+    color: "blue"
+  }
+
+})
